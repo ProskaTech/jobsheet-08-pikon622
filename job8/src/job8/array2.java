4187648aa653b74a8e4bbb613761b6e48a2769a8
Muhammad Farras Fiqhan <112669349+pikon622@users.noreply.github.com>
2022-10-20 20:28:32 +0700

    Add files via upload

---
+++ job8/src/job8/array2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package job8;
+
+/**
+ *
+ * @author Pikonnn
+ */
+public class array2 {
+   public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i < ages.length; i++){
+            System.out.print(ages[i]);
+        }
+    } 
+}
